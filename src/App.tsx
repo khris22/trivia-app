3d32a94136a774a05e18ab73b6d898c36d09deb4
Khris Punzalan <khrispunzalan@gmail.com>
2020-07-18 17:03:56 -0500

    added questionCard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import QuestionCard from './components/QuestionCard';
 
 function App() {
-  return <div className='App'>Random Trivia App</div>;
+  const startTrivia = async () => {};
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+  const nextQuextion = () => {};
+
+  return (
+    <div className='App'>
+      <h1>TRIVIA QUIZ</h1>
+      <button className='start' onClick={startTrivia}>
+        START
+      </button>
+      <p className='score'>Score:</p>
+      <p>Loading Questions...</p>
+      <QuestionCard />
+      <button className='next' onClick={startTrivia}>
+        Next Question
+      </button>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+type Props = {
+  question: string;
+  answer: string[];
+  callback: any;
+  userAnswer: string;
+  questionNum: number;
+  totalQuestions: number;
+};
+
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answer,
+  callback,
+  userAnswer,
+  questionNum,
+  totalQuestions,
+}) => {
+  return (
+    <div>
+      <p className='number'>
+        Question: {questionNum} / {totalQuestions}
+      </p>
+    </div>
+  );
+};
+
+export default QuestionCard;
