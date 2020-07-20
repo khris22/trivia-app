497725175d1bced36ee29a3125c546ebbfe79ca8
Khris Punzalan <khrispunzalan@gmail.com>
2020-07-19 20:42:36 -0500

    Updates

---
+++ precache-manifest.b7ebf0c2c99e003ca8a5465f04c64c01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34506a776d57f816d3e4b0d3bdd03757",
+    "url": "/trivia-app/index.html"
+  },
+  {
+    "revision": "bb449a8f46f6e354ba4b",
+    "url": "/trivia-app/static/js/2.67cd1655.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trivia-app/static/js/2.67cd1655.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6331633018747a3ac665",
+    "url": "/trivia-app/static/js/main.920047bf.chunk.js"
+  },
+  {
+    "revision": "15ec67f38d36a05e8619",
+    "url": "/trivia-app/static/js/runtime-main.51f2d099.js"
+  },
+  {
+    "revision": "cab9194d0d854d7992e21ca74764a7af",
+    "url": "/trivia-app/static/media/zach-lucero.cab9194d.jpg"
+  }
+]);
